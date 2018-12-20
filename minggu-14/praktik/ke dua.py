9143cd02fda97e3b0f84ba36c241c6dfc5482ad8
umbu1997 <38551481+umbu1997@users.noreply.github.com>
2018-12-20 16:54:37 +0700

    Add files via upload

---
+++ minggu-14/praktik/ke dua.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+x = [-3, 5, 7]
+y = [10, 2, 5]
+
+plt.figure(figsize=(15,3))
+plt.plot(x, y)
+plt.xlim(0, 10)
+plt.ylim(-3, 8)
+plt.xlabel('X Axis')
+plt.ylabel('Y axis')
+plt.title('Line Plot')
+plt.suptitle('Figure Title', size=20, y=1.03)
+
+
+# In[ ]:
+
+
+
+
