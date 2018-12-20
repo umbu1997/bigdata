5000d5fe38276048b22145a66ef407b72f7e1735
umbu1997 <38551481+umbu1997@users.noreply.github.com>
2018-12-20 17:03:46 +0700

    Add files via upload

---
+++ minggu-14/praktik/ke 4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[4]:
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1)
+
+
+# In[ ]:
+
+
+
+
